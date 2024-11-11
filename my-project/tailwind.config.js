ab31e1a051a5da6d5c37ba1f44465aa823acae97
MAhmad <ahmadmahi@gmail.com>
2024-11-11 11:19:47 -0800

    first commit

---
+++ my-project/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    module.exports = {
+      purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    }
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
